8d8a115b48f5121a3d333a9d354ac5c9bfbc7c00
조준범 <jojunbeom@jojunbeom-ui-MacBook-Pro.local>
2023-12-01 22:41:00 +0900

    init commit

---
+++ src/algorithm/_01_String/_01_문자찾기/Main.java
@@ -0,0 +1,23 @@
+package algorithm._01_String._01_문자찾기;
+
+import java.util.Scanner;
+
+class Main {
+    public int solution(String Text, char search) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String Text = sc.nextLine();
+        char Search = sc.next().charAt(0);
+
+        Main main = new Main();
+        int answer = main.solution(Text, Search);
+
+        System.out.println(answer);
+    }
+}
+
+
