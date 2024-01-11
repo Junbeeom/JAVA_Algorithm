ca426fa6ffcf2b067f5950924513b4f3465b0ad3
jojunbeom <wns_p@naver.com>
2024-01-12 00:27:20 +0900

    array 1,2

---
+++ src/algorithm/_02_Array/_01_큰_수_출력하기/Main.java
@@ -0,0 +1,22 @@
+package algorithm._02_Array;
+
+import java.util.Scanner;
+
+class Main {
+    public void Solution() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = n; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+        Main main = new Main();
+    }
+}
